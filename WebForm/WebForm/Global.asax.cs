45976da6b20854c1f4cd7b21dfc1560573f52ebd
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-05-23 11:15:05 +0800

    Update

--- WebForm/WebForm/Global.asax.cs
+++ WebForm/WebForm/Global.asax.cs
@@ -50,39 +50,39 @@ namespace WebForm
             Application["message"] = "Application Event";
         }
 
-        protected void Session_Start(object sender, EventArgs e)
-        {
+        //protected void Session_Start(object sender, EventArgs e)
+        //{
 
-        }
+        //}
 
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-            startTime = Context.Timestamp;
-        }
+        //protected void Application_BeginRequest(object sender, EventArgs e)
+        //{
+        //    startTime = Context.Timestamp;
+        //}
 
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
+        //protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        //{
 
-        }
+        //}
 
-        protected void Application_EndRequest(Object sender, EventArgs e)
-        {
-            double elapsed = DateTime.Now.Subtract(startTime).TotalMilliseconds;
-            System.Diagnostics.Debug.WriteLine(String.Format($"Duration : {Request.RawUrl} {elapsed}ms"));
-        }
+        //protected void Application_EndRequest(Object sender, EventArgs e)
+        //{
+        //    double elapsed = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+        //    System.Diagnostics.Debug.WriteLine(String.Format($"Duration : {Request.RawUrl} {elapsed}ms"));
+        //}
 
-        protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
-        {
-            if ((Request.Url.LocalPath == "/Params.aspx") && (!User.Identity.IsAuthenticated))
-            {
-                Context.AddError(new UnauthorizedAccessException());
-            }
-        }
+        //protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        //{
+        //    if ((Request.Url.LocalPath == "/Params.aspx") && (!User.Identity.IsAuthenticated))
+        //    {
+        //        Context.AddError(new UnauthorizedAccessException());
+        //    }
+        //}
 
-        protected void Application_LogRequest(object sender, EventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine($"Request for {Request.RawUrl} - code {Response.StatusCode}");
-        }
+        //protected void Application_LogRequest(object sender, EventArgs e)
+        //{
+        //    System.Diagnostics.Debug.WriteLine($"Request for {Request.RawUrl} - code {Response.StatusCode}");
+        //}
 
         protected void Application_End(object sender, EventArgs e)
         {
