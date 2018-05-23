0c6cf7b584635824a9208ba6ec5da6b6d83fd6b9
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-05-23 15:31:41 +0800

    Update

---
+++ CommonModules/ModuleRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+
+
+[assembly: PreApplicationStartMethod(typeof(CommonModules.ModuleRegistration), "RegisterModules")]
+namespace CommonModules
+{
+    public class ModuleRegistration
+    {
+        public static void RegisterModules()
+        {
+            Type[] moduleTypes = { typeof(LogModule), typeof(TimerModule) };
+            foreach (var module in moduleTypes)
+            {
+                HttpApplication.RegisterModule(module);
+            }
+        }
+    }
+}
