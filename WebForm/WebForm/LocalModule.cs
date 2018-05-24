36c7968157dd43e60b260cf6935aaaa42d0b03e2
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-05-24 14:25:48 +0800

    Update

---
+++ WebForm/WebForm/LocalModule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Web;
+
+namespace WebForm
+{
+    public class LocalModule : IHttpModule
+    {
+        /// <summary>
+        /// You will need to configure this module in the Web.config file of your
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: https://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+            //clean-up code here.
+        }
+
+        public void Init(HttpApplication app)
+        {
+            // Below is an example of how you can handle LogRequest event and provide 
+            // custom logging implementation for it
+            app.LogRequest += new EventHandler(OnLogRequest);
+            app.BeginRequest += HandleEvent;
+        }
+
+        #endregion
+
+        public void OnLogRequest(Object source, EventArgs e)
+        {
+            //custom logging logic can go here
+        }
+
+        protected void HandleEvent(Object sender, EventArgs args)
+        {
+            String[] langs = ((HttpApplication)sender).Request.UserLanguages;
+            if ((langs != null) && (langs.Length > 0) && (langs[0] != null))
+            {
+                try
+                {
+                    Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(langs[0]);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ WebForm/WebForm/Price.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Price.aspx.cs" Inherits="WebForm.Price" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>Today's data is <%: DateTime.Now.ToShortDateString() %></p>
+    <p>A new shirt costs <%: 20.ToString("c") %></p>
+</body>
+</html>
