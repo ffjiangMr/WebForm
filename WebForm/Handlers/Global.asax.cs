dd74b430a5d6a1d5fe25083a9cca9e8fd2ec93f9
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-05-25 15:07:18 +0800

    update

---
+++ WebForm/Handlers/Global.asax.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Handlers
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public Global()
+        {
+            MapRequestHandler += HandleEvent;
+            PostMapRequestHandler += HandleEvent;
+            PreRequestHandlerExecute += HandleEvent;
+            PostRequestHandlerExecute += HandleEvent;
+        }
+
+        private void HandleEvent(Object sender, EventArgs args)
+        {
+            String eventType = Context.CurrentNotification.ToString();
+            if (Context.IsPostNotification)
+            {
+                eventType = "Post" + eventType;
+            }
+            System.Diagnostics.Debug.WriteLine($"Rquest Event: {eventType}");
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebForm/Handlers/Time.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Handlers
+{
+    /// <summary>
+    /// Summary description for Time
+    /// </summary>
+    public class Time : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
