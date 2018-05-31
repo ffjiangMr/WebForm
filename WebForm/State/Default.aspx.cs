e5445031358ce799597d35b4099d15a7bfbb933e
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-05-31 15:53:25 +0800

    update

---
+++ WebForm/State/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO.Ports;
+
+namespace State
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private Int32 counter = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected Int32 GetCounter()
+        {
+            Application.Lock();
+            Int32 result = (Int32)(Application["counter"] ?? 0);
+            Application["counter"] = ++result;
+            Application.UnLock();
+            return result;
+        }
+
+    }
+}
