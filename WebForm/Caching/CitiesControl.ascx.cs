b36b5eb30acf72a5e0f3121e5a23f1fc3afc89f8
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-06-07 17:34:06 +0800

    update

--- WebForm/Caching/CitiesControl.ascx.cs
+++ WebForm/Caching/CitiesControl.ascx.cs
@@ -19,35 +19,61 @@ namespace Caching
     {
         private static readonly String fileName = "/CitiesList.html";
         private static readonly String CACHE_KEY = "cities_html";
+        private static readonly String TIME_CACHE_KEY = "cities_time";
+        private static readonly String HTML_CACHE_KEY = "cities_html";
         private CityListInfo cityInfo;
         private Boolean cached = false;
 
         public String GetCities()
         {
-            return cityInfo.Html;
-        }
-
-        protected String GetTimeStamp()
-        {
-            return cityInfo.Timetamp;
-        }
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            cityInfo = Cache[CACHE_KEY] as CityListInfo;
-            if (cityInfo == null)
+            String html = (String)Cache[HTML_CACHE_KEY];
+            if (html == null)
             {
-                cityInfo = new CityListInfo()
-                {
-                    Timetamp = DateTime.Now.ToLongTimeString(),
-                    Html = File.ReadAllText(MapPath(fileName)),
-                };
-                Cache.Insert(CACHE_KEY, cityInfo, new CacheDependency(MapPath(fileName)));
+                html = File.ReadAllText(MapPath(fileName));
+                AggregateCacheDependency aggDep = new AggregateCacheDependency();
+                aggDep.Add(new CacheDependency(MapPath(fileName)));
+                aggDep.Add(new RequestCountDependency(3));
+                Cache.Insert(HTML_CACHE_KEY, html,
+                    //new CacheDependency(MapPath(fileName))
+                    //new RequestCountDependency(3)
+                    aggDep
+                    );
             }
             else
             {
                 cached = true;
             }
+            return html;
+        }
+
+        protected String GetTimeStamp()
+        {
+            String timeStamp = (String)Cache[TIME_CACHE_KEY];
+            if (timeStamp == null)
+            {
+                timeStamp = DateTime.Now.ToLongTimeString();
+                Cache.Insert(TIME_CACHE_KEY, timeStamp, new CacheDependency(null, new String[] { HTML_CACHE_KEY }));
+            }
+
+            return timeStamp + (cached ? "<b>Cache</b>" : "");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //cityInfo = Cache[CACHE_KEY] as CityListInfo;
+            //if (cityInfo == null)
+            //{
+            //    cityInfo = new CityListInfo()
+            //    {
+            //        Timetamp = DateTime.Now.ToLongTimeString(),
+            //        Html = File.ReadAllText(MapPath(fileName)),
+            //    };
+            //    Cache.Insert(CACHE_KEY, cityInfo, new CacheDependency(MapPath(fileName)));
+            //}
+            //else
+            //{
+            //    cached = true;
+            //}
         }
     }
 }
---
+++ WebForm/Caching/RequestCountDependency.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+
+namespace Caching
+{
+
+    public class RequestEventMapMoudle : IHttpModule
+    {
+        public event EventHandler BeginRequest;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += (src, args) =>
+            {
+                if (BeginRequest != null)
+                {
+                    BeginRequest(this, EventArgs.Empty);
+                }
+            };
+        }
+    }
+
+    public class RequestCountDependency:CacheDependency
+    {
+        private Int32 requestLimit, requestCount;
+
+        public RequestCountDependency(Int32 limit)
+        {
+            this.requestLimit = limit;
+            this.requestCount = 0;
+            ConfigureEventHandler(true);
+            FinishInit();
+        }
+
+        private void ConfigureEventHandler(Boolean attach)
+        {
+            if (HttpContext.Current != null)
+            {
+                RequestEventMapMoudle moudle = HttpContext.Current.ApplicationInstance.Modules["RequestEventMap"] as RequestEventMapMoudle;
+                if (moudle != null)
+                {
+                    if (attach)
+                    {
+                        moudle.BeginRequest += this.HandleEvent;
+                    }
+                    else
+                    {
+                        moudle.BeginRequest -= this.HandleEvent;
+                    }
+                }
+            }
+        }
+
+        private void HandleEvent(Object sender, EventArgs args)
+        {
+            if (++requestCount > requestLimit)
+            {
+                NotifyDependencyChanged(this, EventArgs.Empty);
+            }
+        }
+
+        protected override void DependencyDispose()
+        {
+            ConfigureEventHandler(false);
+            base.DependencyDispose();
+        }
+
+    }
+}
