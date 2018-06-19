67862669e1d57c0e488c9307f8eaefba60b0dcae
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-06-19 19:09:24 +0800

    update

---
+++ WebForm/PathsAnd URLs/Content/RequestReporter.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RequestReporter.aspx.cs" Inherits="PathsAnd_URLs.Content.RequestReporter" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>Original virtual path:<%: Request.FilePath %></p>
+    <p>Original physical path:<%: Request.PhysicalPath %></p>
+    <p>Current virtual path:<%: Request.CurrentExecutionFilePath %></p>
+    <p>Current physical path:<%: Request.MapPath(Request.CurrentExecutionFilePath) %></p>
+</body>
+</html>
---
+++ WebForm/PathsAnd URLs/SimpleModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace PathsAndURLs
+{
+    public class SimpleModule : IHttpModule
+    {
+        /// <summary>
+        /// You will need to configure this module in the Web.config file of your
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: https://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+            //clean-up code here.
+        }
+
+        public void Init(HttpApplication context)
+        {
+            // Below is an example of how you can handle LogRequest event and provide 
+            // custom logging implementation for it
+            context.BeginRequest += (src, args) => { ProcessRequest(context); };
+        }
+
+        private void ProcessRequest(HttpApplication app)
+        {
+            if (app.Request.FilePath == "/Test.aspx")
+            {
+                app.Server.Transfer("Content/RequestReporter.aspx");
+            }
+            WriteMsg("URL requested: {0} {1}", app.Request.RawUrl, app.Request.FilePath);
+        }
+
+        private void WriteMsg(String formatString, params Object[] vals)
+        {
+            Debug.WriteLine(formatString,vals);
+        }
+
+        #endregion
+
+        public void OnLogRequest(Object source, EventArgs e)
+        {
+            //custom logging logic can go here
+        }
+    }
+}
