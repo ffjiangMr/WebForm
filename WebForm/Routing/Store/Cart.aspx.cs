5f074de1b057bbf85168863d0f468f83f76e1235
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-06-20 19:38:44 +0800

    update

---
+++ WebForm/Routing/Store/Cart.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Routing.Store
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected String GetURLFromRoute()
+        {
+            Route myRoute = RouteData.Route as Route;
+            if (myRoute != null)
+            {
+                return myRoute.Url;
+            }
+            else
+            {
+                return "Unknown RouteBase";
+            }
+
+        }
+    }
+}
