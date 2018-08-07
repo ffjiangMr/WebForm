0aceadeb542a366667a96527ed9c449244f746b4
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-08-07 16:40:30 +0800

    update

---
+++ WebForm/ServerSiteHtml/CreateTable.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateTable.aspx.cs" Inherits="ServerSiteHtml.CreateTable" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <style>
+        table {
+            border: thin solid black;
+            border-spacing: 0px;
+            border-collapse: separate;
+            font-family:Monospaced Number,Chinese Quote,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Helvetica Neue,Helvetica,Arial,sans-serif;
+        }
+
+        tr {
+            border: 0px solid black;
+        }
+
+        td, th {
+            border: 1px solid black;
+            font-size:10px;
+        }
+
+        th {
+            font-weight: normal;          
+        }
+
+        thead > tr {
+            border: solid thin black;
+        }
+
+        td:last-child, th:last-child {
+            text-align: right;
+        }
+
+        span {
+            font: normal;
+        }
+
+        div {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="container" runat="server">
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm/ServerSiteHtml/CreateTable.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using Control = System.Web.UI.HtmlControls;
+
+namespace ServerSiteHtml
+{
+    public partial class CreateTable : System.Web.UI.Page
+    {
+        private List<String[]> tableRows = new List<String[]>()
+                                           {
+                                               new String[]{"Red","2" },
+                                               new String[]{"Greent","41" },
+                                               new String[]{"Blue","3" },
+                                           };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Control.HtmlTable table = new Control.HtmlTable();
+            Control.HtmlTableRow headerRow = new Control.HtmlTableRow();
+            headerRow.Cells.Add(new Control.HtmlTableCell("th") { InnerText = "Color" });
+            headerRow.Cells.Add(new Control.HtmlTableCell("th") { InnerText = "Count" });
+            table.Rows.Add(headerRow);
+            foreach (String[] data in tableRows)
+            {
+                table.Rows.Add(new Control.HtmlTableRow
+                {
+                    Cells = {
+                        new Control.HtmlTableCell{ InnerText = data[0]},
+                        new Control.HtmlTableCell{ InnerText = data[1]},
+                    }
+                });
+            }
+
+            Control.HtmlTableRow footRow = new Control.HtmlTableRow();
+            footRow.Cells.Add(new Control.HtmlTableCell("th") { InnerText = "Total" });
+            footRow.Cells.Add(new Control.HtmlTableCell("th") { InnerText = "46" });
+            table.Rows.Add(footRow);
+
+            container.Controls.Add(table);
+        }
+    }
+}
